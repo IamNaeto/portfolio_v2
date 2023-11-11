814d5be78f97c9ab17ecdd62e91b61c191f2c056
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2023-11-11 05:51:02 -0800

    adjustments

--- src/app/components/ProfileBar.tsx
+++ src/app/components/ProfileBar.tsx
@@ -14,7 +14,7 @@ const ProfileBar = () => {
       };
       
     return (
-        <aside className="relative xl:fixed w-full xl:w-[24%] bg-eerie-black rounded-lg border border-jet border-solid shadow-sm shadow-jet p-5 flex flex-col gap-4 mt-5 md:mt-0">
+        <aside className="relative xl:fixed w-full xl:w-[24%] bg-eerie-black rounded-lg border border-jet border-solid shadow-sm shadow-jet py-3 px-5 flex flex-col gap-4 mt-5 md:mt-0">
             <div className="flex flex-row xl:flex-col items-center justify-start xl:justify-center gap-2">
                 <div className="w-[25%] xl:w-full flex items-center justify-center">
                     <Image src="/img/Naeto.jpg" width={200} height={200} alt="Charles O. Egesionu Image" className="w-full xl:w-[60%] rounded-2xl" />
@@ -31,21 +31,21 @@ const ProfileBar = () => {
             <div className={`${isMenuOpen ? 'flex' : 'hidden'} xl:flex flex-col gap-4`}>
                 <div className="flex flex-col gap-2 items-start border-t-[2px] border-b-[2px] border-jet border-solid pt-3 pb-3 overflow-x-hidden">
                     <div className="flex items-center justify-center gap-4">
-                        <HiOutlineMail className="p-2 rounded-xl text-vegas-gold bg-smoky-black border border-jet border-solid text-4xl" />
+                        <HiOutlineMail className="p-2 rounded-xl text-vegas-gold bg-smoky-black border border-jet border-solid text-3xl" />
                         <a href="">
                             <p className="text-gray text-[12px] font-bold">Email</p>
                             <h4 className="text-[11px] text-litewhite">egesionucharlesobimnaeto@gmail.com</h4>
                         </a>
                     </div>
                     <div className="flex items-center justify-center gap-4">
-                        <HiOutlineDevicePhoneMobile className="p-2 rounded-xl text-vegas-gold bg-smoky-black border border-jet border-solid text-4xl" />
+                        <HiOutlineDevicePhoneMobile className="p-2 rounded-xl text-vegas-gold bg-smoky-black border border-jet border-solid text-3xl" />
                         <a href="">
                             <p className="text-gray text-[12px] font-bold">Phone</p>
                             <h4 className="text-[11px] text-litewhite">+2348147371491</h4>
                         </a>
                     </div>
                     <div className="flex items-center justify-center gap-4">
-                        <HiOutlineMapPin className="p-2 rounded-xl text-vegas-gold bg-smoky-black border border-jet border-solid text-4xl" />
+                        <HiOutlineMapPin className="p-2 rounded-xl text-vegas-gold bg-smoky-black border border-jet border-solid text-3xl" />
                         <div>
                             <p className="text-gray text-[12px] font-bold">Location</p>
                             <h4 className="text-[11px] text-litewhite">Imo State, Nigeria</h4>
@@ -55,10 +55,10 @@ const ProfileBar = () => {
                 </div>
                 <div className="flex items-center justify-center">
                     <div className="flex items-center justify-center gap-4">
-                        <a href=""><FaGithub className="text-4xl p-2 rounded-xl bg-smoky-black hover:bg-jet transition-all delay-200 border border-jet border-solid  text-litewhite hover:text-vegas-gold font-medium" /></a>
-                        <a href=""><FaXTwitter className="text-4xl p-2 rounded-xl bg-smoky-black hover:bg-jet transition-all delay-200 border border-jet border-solid  text-litewhite hover:text-vegas-gold font-medium" /></a>
-                        <a href=""><FaLinkedinIn className="text-4xl p-2 rounded-xl bg-smoky-black hover:bg-jet transition-all delay-200 border border-jet border-solid  text-litewhite hover:text-vegas-gold font-medium" /></a>
-                        <a href=""><FaWhatsapp className="text-4xl p-2 rounded-xl bg-smoky-black hover:bg-jet transition-all delay-200 border border-jet border-solid  text-litewhite hover:text-vegas-gold font-medium" /></a>
+                        <a href=""><FaGithub className="text-3xl p-2 rounded-xl bg-smoky-black hover:bg-jet transition-all delay-200 border border-jet border-solid  text-litewhite hover:text-vegas-gold font-medium" /></a>
+                        <a href=""><FaXTwitter className="text-3xl p-2 rounded-xl bg-smoky-black hover:bg-jet transition-all delay-200 border border-jet border-solid  text-litewhite hover:text-vegas-gold font-medium" /></a>
+                        <a href=""><FaLinkedinIn className="text-3xl p-2 rounded-xl bg-smoky-black hover:bg-jet transition-all delay-200 border border-jet border-solid  text-litewhite hover:text-vegas-gold font-medium" /></a>
+                        <a href=""><FaWhatsapp className="text-3xl p-2 rounded-xl bg-smoky-black hover:bg-jet transition-all delay-200 border border-jet border-solid  text-litewhite hover:text-vegas-gold font-medium" /></a>
                     </div>
                 </div>
             </div>
--- src/app/journey/page.tsx
+++ src/app/journey/page.tsx
@@ -5,7 +5,7 @@ import MyJourney from "../components/MyJourney"
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen h-full px-[3%] py-[2%] bg-smoky-black gap-10">
+    <main className="w-full min-h-screen h-full px-[2%] py-[1%] bg-smoky-black gap-10">
       <ProfileBar />
       <MyJourney />
     </main>
