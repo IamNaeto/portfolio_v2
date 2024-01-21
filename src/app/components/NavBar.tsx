9560f03b176dad8c3fe7eb2151730fe66f43ad04
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-01-20 20:21:24 -0800

    minor adjust

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
     const pathName = usePathname()
     return (
         <header className="fixed lg:absolute bottom-0 lg:bottom-auto left-0 lg:left-auto lg:top-0 lg:right-0 flex items-center lg:items-end justify-center lg:justify-end w-full lg:w-[400px] rounded-lt-lg  rounded-rt-lg z-50">
-            <nav className="relative flex items-center justify-center text-[16px] font-medium text-litewhite gap-5 sm:gap-8 lg:gap-6 rounded-bl-none lg:rounded-bl-lg rounded-tr-lg lg:rounded-tr-lg rounded-tl-lg lg:rounded-tl-none  border border-jet border-solid p-4 backdrop-blur-lg bg-transparent lg:bg-smoky-gray shadow-sm w-full z-36">
+            <nav className="relative flex items-center justify-center text-[14px] sm:text-[16px] font-medium text-litewhite gap-4 sm:gap-8 lg:gap-6 rounded-bl-none lg:rounded-bl-lg rounded-tr-lg lg:rounded-tr-lg rounded-tl-lg lg:rounded-tl-none  border border-jet border-solid p-4 backdrop-blur-lg bg-transparent lg:bg-smoky-gray shadow-sm w-full z-36">
                 <Link href="/" className={pathName === "/" ? "flex items-center gap-1 visited:text-vegas-gold font-bold transition-all delay-200" : "flex items-center gap-1 hover:text-gray transition-all delay-200"}><FaUser /> About</Link>
                 <Link href="/journey" className={pathName === "/journey" ? "flex items-center gap-1 visited:text-vegas-gold font-bold transition-all delay-200" : "flex items-center gap-1 hover:text-gray transition-all delay-200"}><GoChecklist /> Journey</Link>
                 <Link href="/projects" className={pathName === "/projects" ? "flex items-center gap-1 visited:text-vegas-gold font-bold transition-all delay-200" : "flex items-center gap-1 hover:text-gray transition-all delay-200"}><AiOutlineFundProjectionScreen /> Projects</Link>
